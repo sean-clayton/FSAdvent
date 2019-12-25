1988666026c5fd98a01367a1e4eeb8140327b2a9
Sean Clayton <clayton.sean@gmail.com>
2019-12-25 12:43:36 -0500

    Add day 2

---
+++ Advent.Y2019/Day2.fs
@@ -0,0 +1,50 @@
+﻿namespace Advent.Y2019
+
+open Advent.Common.Project
+
+module Day2 =
+    type Program = int array
+
+    let input = parseFileLinesBy "../InputFiles/Day2.txt" (fun s -> s.Split(',') |> Array.map int) |> Seq.head
+
+    let performOperation f x y resultIndex (program: Program) =
+        program.[resultIndex] <- f x y
+
+    let rec executeProgram index (program: Program) =
+        let operation = program.[index]
+        let x = program.[program.[index + 1]]
+        let y = program.[program.[index + 2]]
+        let resultIndex = program.[index + 3]
+        let nextIndex = index + 4
+
+        match operation with
+        | 1 ->
+            performOperation (+) x y resultIndex program
+            executeProgram nextIndex program
+        | 2 ->
+            performOperation (*) x y resultIndex program
+            executeProgram nextIndex program
+        | _ -> ()
+
+    let runPart2 noun verb =
+        let program = Array.copy input
+        program.[1] <- noun
+        program.[2] <- verb
+
+        executeProgram 0 program
+
+        match program.[0] with
+        | 19690720 -> Some (100 * noun + verb)
+        | _ -> None
+
+    let part1 () =
+        let program = Array.copy input
+        program.[1] <- 12
+        program.[2] <- 2
+
+        executeProgram 0 program
+        program.[0]
+
+    let part2 () =
+        [0..99] |> Seq.collect (fun noun ->
+            [0..99] |> Seq.map (fun verb -> runPart2 noun verb)) |> Seq.pick id
--- Advent.Y2019/Program.fs
+++ Advent.Y2019/Program.fs
@@ -6,4 +6,6 @@ open Advent.Y2019
 let main argv =
     Day1.part1() |> printfn "Day 1, Part 1 solution: %i"
     Day1.part2() |> printfn "Day 1, Part 2 solution: %i"
+    Day2.part1() |> printfn "Day 2, Part 1 solution: %i"
+    Day2.part2() |> printfn "Day 2, Part 2 solution: %i"
     0
